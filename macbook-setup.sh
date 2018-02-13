dbfa2aca9ae5dcff2102bdb4acf2c2a933f58abf
mcpengelly <pengelly.mat@gmail.com>
2018-02-13 09:36:57 -0500

    reorganize script, add atom texteditor, leave notes in script for other users

--- macbook-setup.sh
+++ macbook-setup.sh
@@ -1,7 +1,7 @@
-# homebrew
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# homebrew cask
+# get homebrew cask
 brew tap caskroom/cask
 
 # chrome
@@ -13,45 +13,20 @@ brew cask install slack
 # nvm install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-# text editor
-brew cask install sublime-text
-
-# custom terminal
-brew cask install iterm2
-
-# custom hotkey app
-brew cask install bettertouchtool
-
-# custom calendar app
-brew cask install itsycal
-
-# backend development tool
-brew cask install webstorm
-
-# custom terminal
-brew cask install iterm2
-
-# REST Client
-brew cask install postman
-
-# keepalive app
-brew cask install caffeine
-
-# music
-brew cask install spotify
-
-# chat clients
-brew cask install discord
-brew cask install whatsapp
-
-# node install LTS
+# node install long term support version
 nvm install stable
 nvm use stable
 
-# if you like to use bash uncoment the following: and comment out oh my zsh / .zshrc
+## NOTE: only bash_profile or oh my zsh setup should be used, not both.
+
+######### standard bash profile setup ########
+# if you like to use the bash terminal uncoment the following: and comment out oh my zsh / .zshrc
 # touch ~/.bash_profile
 # echo "[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh" >> ~/.bash_profile
+######### standard bash profile setup ########
 
+
+######### oh my zsh terminal profile setup + theme ########
 # oh my zsh style for iterm2
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -63,12 +38,47 @@ ZSH_THEME="agnoster/agnoster"
 
 #  adds nvm to end of ~/.zshrc configuration so whenever terminal is reset nvm gets reloaded
 echo "[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh" >> ~/.zshrc
+######### oh my zsh terminal profile setup ########
 
 # vim config download
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
 # vim config install
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+
+####### PREFERENCE STUFF, UNCOMMENT/COMMENT DESIRED TOOLS HERE #######
+
+# text editors, pick your preference
+brew cask install sublime-text
+brew cask install atom
+
+# javasceript IDE (useful for backend debugging)
+brew cask install webstorm
+
+# better terminal
+brew cask install iterm2
+
+# custom hotkey app (allows window snapping by default)
+brew cask install bettertouchtool
+
+# custom calendar app (easier to see the date in toolbar)
+brew cask install itsycal
+
+# REST Client for making backend requests
+brew cask install postman
+
+# keepalive app for your macbook
+brew cask install caffeine
+
+# music
+brew cask install spotify
+
+# chat clients
+brew cask install discord
+brew cask install whatsapp
+
+#################################
 
 # node help app
 npm i -g tldr
