c536cd4abc00e440d9d88b71781216c478b9a950
Matthew Pengelly <pengelly.mat@gmail.com>
2018-08-17 07:44:41 -0400

    Update macbook-setup.sh

--- macbook-setup.sh
+++ macbook-setup.sh
@@ -10,18 +10,14 @@ brew cask install google-chrome
 # slack
 brew cask install slack
 
-# cmd line fuzzy searcher
-brew install fzf
-
-# better ack
-brew install the_silver_searcher
-
 # nvm install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 # node install long term support version
 nvm install stable
 nvm use stable
+
+# PREFERENCE STUFF, UNCOMMENT/COMMENT DESIRED TOOLS HERE
 
 ## NOTE: only bash_profile or oh my zsh setup should be used, not both.
 
@@ -36,11 +32,9 @@ nvm use stable
 # oh my zsh style for iterm2
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# oh my zsh theme
-git clone https://github.com/agnoster/agnoster-zsh-theme.git ~/.oh-my-zsh/custom/themes/agnoster
-
 # apply theme
-ZSH_THEME="agnoster/agnoster"
+export ZSH_THEME="robbyrussell" # simple, works out of the box
+# export ZSH_THEME="agnoster" # more advanced but uses powerline fonts
 
 #  adds nvm to end of ~/.zshrc configuration so whenever terminal is reset nvm gets reloaded
 echo "[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh" >> ~/.zshrc
@@ -52,19 +46,21 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 # vim config install
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-
-# PREFERENCE STUFF, UNCOMMENT/COMMENT DESIRED TOOLS HERE
-#################################
-
 # text editors, pick your preference
-brew cask install sublime-text
-brew cask install atom
+# brew cask install sublime-text
+# brew cask install atom
+brew cask visual-studio-code
 
-# javasceript IDE (useful for backend debugging)
-brew cask install webstorm
+# powerline patched fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
 
-# better terminal
+# better terminal, be sure to set the font to a powerline font
 brew cask install iterm2
+
+# REST Client for making backend requests
+brew cask install postman
 
 # custom hotkey app (allows window snapping by default)
 brew cask install bettertouchtool
@@ -72,8 +68,11 @@ brew cask install bettertouchtool
 # custom calendar app (easier to see the date in toolbar)
 brew cask install itsycal
 
-# REST Client for making backend requests
-brew cask install postman
+# cmd line fuzzy searcher
+brew install fzf
+
+# better ack
+brew install the_silver_searcher
 
 # keepalive app for your macbook
 brew cask install caffeine
@@ -91,10 +90,7 @@ brew cask install drawio
 # clipboard manager
 brew cask install copyq
 
-#################################
-
-# npm terminal programs i like to use
-#################################
+### NPM Terminal Programs
 
 # node help app
 npm i -g tldr
@@ -127,7 +123,24 @@ npm i -g how2
 # generate changelogs from git metadata
 npm i -g conventional-changelog-cli
 
-#################################
-#
+# oh my zsh plugins, just copy paste these into your .zshrc afterward
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+# plugins=(
+  # git
+  # git-extras
+  # brew
+  # jira
+  # vscode
+  # web-search
+  # yarn
+  # jsontools
+  # node
+  # osx
+  # common-aliases
+  # encode64
+# )
 
 
